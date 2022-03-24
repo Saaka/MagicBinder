da4be8bae79bb3d381d4eab87d5d1490d2203fb5
Michał Sakwa <sakwa.michal@gmail.com>
2022-03-24 20:27:44 +0100

    Added Serilog.

--- MagicBinder.WebApi/Program.cs
+++ MagicBinder.WebApi/Program.cs
@@ -1,12 +1,19 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using MagicBinder.WebApi.CompositionRoots;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>(cb => cb.RegisterAppModules());
 
 // Add services to the container.
+
+builder.Logging.ClearProviders();
+ILogger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+builder.Services.AddSingleton(logger);
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
