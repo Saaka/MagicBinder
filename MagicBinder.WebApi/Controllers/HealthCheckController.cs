f0a7b2223c29504792f04042f9914f5a84d4fe11
Michał Sakwa <sakwa.michal@gmail.com>
2022-03-24 20:02:27 +0100

    Removed default controller.

---
+++ MagicBinder.WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicBinder.WebApi.Controllers;
+
+[ApiController]
+[Route("health-check")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly ILogger<HealthCheckController> _logger;
+
+    public HealthCheckController(ILogger<HealthCheckController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("OK");
+    }
+}
