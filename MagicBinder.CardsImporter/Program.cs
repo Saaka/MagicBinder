300eff9bc15e2973a66f3902436d94731a1ef2d1
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-03 23:21:28 +0200

    Added logger to importer..

--- MagicBinder.CardsImporter/Program.cs
+++ MagicBinder.CardsImporter/Program.cs
@@ -6,6 +6,7 @@ using MagicBinder.CardsImporter.CompositionRoots;
 using MagicBinder.CardsImporter.CompositionRoots.Extensions;
 using MagicBinder.CardsImporter.Services;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 const string defaultFileName = "import.json";
 try
@@ -16,13 +17,15 @@ try
 
     await using var scope = container.BeginLifetimeScope()
         .AddHangfire();
+
+    var logger = container.Resolve<ILogger<Program>>();
     
-    Console.WriteLine("Please provide file name to import (default is '{0}'):", defaultFileName);
+    logger.LogInformation("Please provide file name to import (default is '{0}'):", defaultFileName);
     var fileName = Console.ReadLine();
     fileName = string.IsNullOrWhiteSpace(fileName) ? defaultFileName : fileName;
     
     var jsonService = scope.Resolve<CardsJsonService>();
-    Console.WriteLine("Reading file '{0}'...", fileName);
+    logger.LogInformation("Reading file '{0}'...", fileName);
     var json = await jsonService.GetJson(fileName);
 
     var mediator = scope.Resolve<IMediator>();
