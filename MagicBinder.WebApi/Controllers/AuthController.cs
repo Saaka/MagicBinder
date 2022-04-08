cd282e3bdb436a3e1448d19f2aea1274a881c02e
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-09 00:05:06 +0200

    Added AuthController.

---
+++ MagicBinder.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicBinder.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("google")]
+    public async Task<ActionResult> AuthorizeWithGoogle()
+    {
+        throw new NotImplementedException();
+    }
+}
