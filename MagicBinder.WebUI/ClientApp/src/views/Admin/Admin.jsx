b6674f4d8fa177bf4fafcc7d6e45ce9d3f6077b8
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-21 23:11:16 +0200

    Remove old admin buttons.

--- MagicBinder.WebUI/ClientApp/src/views/Admin/Admin.jsx
+++ MagicBinder.WebUI/ClientApp/src/views/Admin/Admin.jsx
@@ -18,27 +18,10 @@ function Admin(props) {
                 <p className="title has-text-light">Admin Page</p>
                 <div className="tile is-ancestor">
                     <div className="tile is-parent">
-                        <div className="tile box" onClick={() => redirectTo(RouteNames.AdminProducts)}>
+                        <div className="tile box" onClick={() => redirectTo(RouteNames.Home)}>
                             <div className="content">
-                                <p className="title">Products</p>
-                                <p className="subtitle">Manage categories, configure the nutritional values and units of
-                                    measures available for each product</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="tile is-parent">
-                        <div className="tile box" onClick={() => redirectTo(RouteNames.AdminUnitsOfMeasures)}>
-                            <div className="content">
-                                <p className="title">Units of measures</p>
-                                <p className="subtitle">Setup units of measures</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="tile is-parent">
-                        <div className="tile box" onClick={() => redirectTo(RouteNames.AdminProductCategories)}>
-                            <div className="content">
-                                <p className="title">Product Categories</p>
-                                <p className="subtitle">Manage product categories</p>
+                                <p className="title">Home</p>
+                                <p className="subtitle">Just go back to home page!</p>
                             </div>
                         </div>
                     </div>
