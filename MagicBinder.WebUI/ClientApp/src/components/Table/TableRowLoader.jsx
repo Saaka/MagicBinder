ba49d34dc508f26ac042ebc6f63447f7dce5288f
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-23 01:18:58 +0200

    Added loader with empty table data entries to avoid uneven render.

--- MagicBinder.WebUI/ClientApp/src/components/Table/TableRowLoader.jsx
+++ MagicBinder.WebUI/ClientApp/src/components/Table/TableRowLoader.jsx
@@ -4,18 +4,31 @@ import "./TableRowLoader.scss";
 
 const TableRowLoader = (props) => {
 
-    const getColumnsCount = () => !!props.columns ? props.columns - 1 : 0;
-    
+    const getColumnsCount = () =>
+        !props.columns || props.columns === 1 ? 0 :
+            props.columns % 2 > 0 ? (props.columns - 1) / 2 : props.columns / 2;
+
+    const shouldRenderProceedingColumns = () => !!props.columns && props.columns > 2;
+
     return (
         <tr className="table-row-loader">
-            <td>
+            {
+                shouldRenderProceedingColumns() ?
+                    [...Array(getColumnsCount())].map((e, i) =>
+                        <td key={i + 1}>
+                            <div><span/></div>
+                        </td>
+                    )
+                    : <React.Fragment/>
+            }
+            <td key="loader">
                 <div className="center">
-                    <progress className="progress is-small is-primary" max="100" />
+                    <progress className="progress is-small is-primary" max="100"/>
                 </div>
             </td>
             {
                 [...Array(getColumnsCount())].map((e, i) =>
-                    <td>
+                    <td key={~(i + 1)}>
                         <div><span/></div>
                     </td>
                 )
