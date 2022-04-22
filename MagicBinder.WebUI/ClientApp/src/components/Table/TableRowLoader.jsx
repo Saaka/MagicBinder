a7e520be91f3fb703fe9742a49473578354ba35e
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-23 01:04:24 +0200

    Added progress bar to table loader.

--- MagicBinder.WebUI/ClientApp/src/components/Table/TableRowLoader.jsx
+++ MagicBinder.WebUI/ClientApp/src/components/Table/TableRowLoader.jsx
@@ -4,10 +4,15 @@ import "./TableRowLoader.scss";
 
 const TableRowLoader = (props) => {
 
-    const getColumnsCount = () => !!props.columns ?  props.columns : 1;
+    const getColumnsCount = () => !!props.columns ? props.columns - 1 : 0;
     
     return (
         <tr className="table-row-loader">
+            <td>
+                <div className="center">
+                    <progress className="progress is-small is-primary" max="100" />
+                </div>
+            </td>
             {
                 [...Array(getColumnsCount())].map((e, i) =>
                     <td>
