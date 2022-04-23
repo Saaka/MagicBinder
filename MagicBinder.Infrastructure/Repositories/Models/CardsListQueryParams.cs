401e788e04cf136bbab0032d14caeb09ebf806f1
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-24 01:58:58 +0200

    Added query list params.

---
+++ MagicBinder.Infrastructure/Repositories/Models/CardsListQueryParams.cs
@@ -0,0 +1,12 @@
+﻿using MagicBinder.Core.Models;
+
+namespace MagicBinder.Infrastructure.Repositories.Models;
+
+public record CardsListQueryParams : IPageableRequest
+{
+    public string? Name { get; set; }
+    public string? TypeLine { get; set; }
+    public string? OracleText { get; set; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+}
