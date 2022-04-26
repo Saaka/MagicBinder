2565f8fe4a1b1a03f8ca32cdca9c8caeec85a792
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-26 23:49:33 +0200

    Added inventory aggregate.

---
+++ MagicBinder.Domain/Aggregates/Entities/UserCard.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicBinder.Domain.Aggregates.Entities;
+
+public class UserCard
+{
+    public Guid CardId { get; set; }
+    public int Count { get; set; }
+    public string Set { get; set; }
+    public string SetName { get; set; }
+    public string CollectorNumber { get; set; }
+    public string Language { get; set; }
+    public bool IsFoil { get; set; }
+    public string Image { get; set; }
+}
---
+++ MagicBinder.Domain/Aggregates/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using MagicBinder.Domain.Aggregates.Entities;
+
+namespace MagicBinder.Domain.Aggregates;
+
+public class Inventory
+{
+    public Guid OracleId { get; set; }
+    public Guid UserGuid { get; set; }
+    public int TotalCount { get; set; }
+    public List<UserCard> Cards { get; set; } = new();
+}
