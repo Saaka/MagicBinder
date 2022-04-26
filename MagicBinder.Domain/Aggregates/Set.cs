0c5a75ed183127b7dda3daa6432ca6490528e43b
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-27 00:44:31 +0200

    Added set aggregate.

---
+++ MagicBinder.Domain/Aggregates/Set.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicBinder.Domain.Aggregates;
+
+public class Set
+{
+    public Guid SetId { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string ScryfalUri { get; set; }
+    public string ReleasedAt { get; set; }
+    public int CardCount { get; set; }
+    public string SetType { get; set; }
+    public string Icon { get; set; }
+}
