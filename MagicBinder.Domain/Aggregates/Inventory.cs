39e3512f371219c9aa91f36191c83331d00af8c7
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-27 00:07:09 +0200

    Added method to add card to inventory.

--- MagicBinder.Domain/Aggregates/Inventory.cs
+++ MagicBinder.Domain/Aggregates/Inventory.cs
@@ -4,8 +4,28 @@ namespace MagicBinder.Domain.Aggregates;
 
 public class Inventory
 {
-    public Guid OracleId { get; set; }
-    public Guid UserGuid { get; set; }
-    public int TotalCount { get; set; }
-    public List<UserCard> Cards { get; set; } = new();
+    public Inventory(Guid oracleId, Guid userGuid) => (OracleId, UserGuid) = (oracleId, userGuid);
+
+    private Inventory()
+    {
+    }
+
+    public Guid OracleId { get; private set; }
+    public Guid UserGuid { get; private set; }
+    public int TotalCount { get; private set; }
+    public List<UserCard> Cards { get; private set; } = new();
+
+    public Inventory AddCard(UserCard card)
+    {
+        var cardPrinting = GetSameVersion(card);
+        if (cardPrinting == null)
+            Cards.Add(card);
+        else
+            cardPrinting.Count += card.Count;
+
+        TotalCount = Cards.Sum(x => x.Count);
+        return this;
+    }
+
+    private UserCard? GetSameVersion(UserCard card) => Cards.FirstOrDefault(x => x.CardId == card.CardId && x.IsFoil == card.IsFoil);
 }
