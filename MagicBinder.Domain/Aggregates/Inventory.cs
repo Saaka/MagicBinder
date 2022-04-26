27101d3582a6154319e544c4b537211f066be166
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-27 00:27:17 +0200

    Removed obsolete inventory field and methods.

--- MagicBinder.Domain/Aggregates/Inventory.cs
+++ MagicBinder.Domain/Aggregates/Inventory.cs
@@ -4,28 +4,7 @@ namespace MagicBinder.Domain.Aggregates;
 
 public class Inventory
 {
-    public Inventory(Guid oracleId, Guid userGuid) => (OracleId, UserGuid) = (oracleId, userGuid);
-
-    private Inventory()
-    {
-    }
-
-    public Guid OracleId { get; private set; }
-    public Guid UserGuid { get; private set; }
-    public int TotalCount { get; private set; }
-    public List<UserCard> Cards { get; private set; } = new();
-
-    public Inventory AddCard(UserCard card)
-    {
-        var cardPrinting = GetSameVersion(card);
-        if (cardPrinting == null)
-            Cards.Add(card);
-        else
-            cardPrinting.Count += card.Count;
-
-        TotalCount = Cards.Sum(x => x.Count);
-        return this;
-    }
-
-    private UserCard? GetSameVersion(UserCard card) => Cards.FirstOrDefault(x => x.CardId == card.CardId && x.IsFoil == card.IsFoil);
+    public Guid OracleId { get; set; }
+    public Guid UserGuid { get; set; }
+    public List<UserCard> Cards { get; set; } = new();
 }
