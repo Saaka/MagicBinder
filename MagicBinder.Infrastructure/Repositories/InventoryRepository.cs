d6c2416be066468f16986d92851396eab1444610
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-27 00:36:22 +0200

    Added Inventory repository.

---
+++ MagicBinder.Infrastructure/Repositories/InventoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using MagicBinder.Domain.Aggregates;
+using MongoDB.Driver;
+
+namespace MagicBinder.Infrastructure.Repositories;
+
+public class InventoryRepository : IMongoRepository
+{
+    private readonly IMongoDatabase _database;
+
+    public InventoryRepository(IMongoDatabase database)
+    {
+        _database = database;
+    }
+
+    public virtual async Task UpsertAsync(Inventory inventory) => await Inventory
+        .ReplaceOneAsync(x => x.UserGuid == inventory.UserGuid && x.OracleId == inventory.OracleId, inventory, new ReplaceOptions { IsUpsert = true });
+    
+    public IMongoCollection<Inventory> Inventory => _database.GetCollection<Inventory>("Inventory");
+}
