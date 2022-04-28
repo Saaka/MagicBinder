e177d0bc1a3cc35455144ffe18f14dc8e0ec70e8
Michał Sakwa <m.sakwa.ext@rydoo.com>
2022-04-28 20:58:31 +0200

    Added endpoint to download sets.

---
+++ MagicBinder.Application/Mappers/SetsMapper.cs
@@ -0,0 +1,14 @@
+﻿using MagicBinder.Application.Models.Dictionaries;
+using MagicBinder.Domain.Aggregates;
+
+namespace MagicBinder.Application.Mappers;
+
+public static class SetsMapper
+{
+    public static SetModel MapToModel(this Set set) => new()
+    {
+        SetId = set.SetId,
+        Code = set.Code,
+        Name = set.Name
+    };
+}
---
+++ MagicBinder.WebApi/Controllers/DictionariesController.cs
@@ -0,0 +1,21 @@
+﻿using MagicBinder.Application.Models.Dictionaries;
+using MagicBinder.Application.Queries.Dictionaries;
+using MagicBinder.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicBinder.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DictionariesController: ControllerBase
+{
+    private readonly MediatorCommandSender _mediator;
+
+    public DictionariesController(MediatorCommandSender mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet("sets")]
+    public async Task<ActionResult<ICollection<SetModel>>> GetCardDetails() => await _mediator.SendQuery(new GetSetsQuery());
+}
