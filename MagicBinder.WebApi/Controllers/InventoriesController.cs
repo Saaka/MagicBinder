f1d59ac05ae0fcde2c83ad28a5590bd117544513
Michał Sakwa <m.sakwa.ext@rydoo.com>
2022-04-28 23:45:14 +0200

    Added inventories controller.

--- MagicBinder.WebApi/Controllers/InventoriesController.cs
+++ MagicBinder.WebApi/Controllers/InventoriesController.cs
@@ -1,6 +1,22 @@
-﻿namespace MagicBinder.WebApi.Controllers;
+﻿using MagicBinder.Application.Commands.Inventories;
+using MagicBinder.WebApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
+namespace MagicBinder.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
 public class InventoriesController
 {
-    
+    private readonly MediatorCommandSender _mediator;
+
+    public InventoriesController(MediatorCommandSender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPut]
+    [Authorize]
+    public async Task<ActionResult> SaveInventory(SaveCardInventory command) => await _mediator.SendCommand(command);
 }
