17453240815869728b0baa133656820fe3b82712
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-28 22:47:44 +0200

    Added abstract NotFoundException.

---
+++ MagicBinder.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace MagicBinder.Application.Exceptions;
+
+public abstract class NotFoundException : Exception
+{
+    public object Id { get; }
+    public string Type { get; }
+
+    protected NotFoundException(object id, string type) : base($"{type} {id} not found")
+    {
+        Id = id;
+        Type = type;
+    }
+}
+
+public class CardNotFoundException : NotFoundException
+{
+    public CardNotFoundException(Guid oracleId) : base(oracleId, "Card")
+    {
+    }
+}
