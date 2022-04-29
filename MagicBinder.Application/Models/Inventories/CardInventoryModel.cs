cf06adfc307aee497bbc1c1932ff306403044122
Michał Sakwa <m.sakwa.ext@rydoo.com>
2022-04-29 20:22:33 +0200

    Added inventory query.

---
+++ MagicBinder.Application/Models/Inventories/CardInventoryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MagicBinder.Application.Models.Inventories;
+
+public class CardInventoryModel
+{
+    public Guid OracleId { get; set; }
+    public List<InventoryPrintingModel> Printings { get; set; }
+
+    public class InventoryPrintingModel
+    {
+        public Guid? CardId { get; set; }
+        public int Count { get; set; }
+        public bool IsFoil { get; set; }
+    }
+}
