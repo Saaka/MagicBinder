1991db847942955c8758fa453a90b5d6710653db
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-30 00:35:22 +0200

    Added columns to card page.

--- MagicBinder.WebUI/ClientApp/src/views/CardsDatabase/CardPage/CardPage.jsx
+++ MagicBinder.WebUI/ClientApp/src/views/CardsDatabase/CardPage/CardPage.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import {useDocumentTitle} from "Hooks";
 import {CardsService} from "Services";
 import {Loader} from "../../../components/Loader/Loader";
+import "./CardPage.scss";
 
 
 const CardPage = (props) => {
@@ -25,6 +26,41 @@ const CardPage = (props) => {
 
     const renderLoader = () => <div className="center"><Loader size="xs" dark/></div>;
 
+    const renderCardDetails = () => (
+        <div className="card-info">
+            <div className="block subtitle-block">
+                <p className="subtitle">Details</p>
+            </div>
+            <div className="block">
+                <p className="is-6 info-header"><b>Mana cost</b></p>
+                <p>{card.manaCost}</p>
+            </div>
+            <hr/>
+            <div className="block">
+                <p className="is-6"><b>Type line</b></p>
+                <p>{card.typeLine}</p>
+            </div>
+            <hr/>
+            <div className="block">
+                <p className="is-6"><b>Oracle text</b></p>
+                <p>{card.oracleText}</p>
+            </div>
+        </div>
+    );
+
+    const renderCardInventory = () => (
+        <div className="card-info">
+            <div className="block subtitle-block">
+                <p className="subtitle">User Inventory</p>
+            </div>
+            <div className="block">
+                <p className="is-6 info-header"><b>Placeholder</b></p>
+                <p>{card.setName}</p>
+            </div>
+            <hr/>
+        </div>
+    );
+
     const renderCard = () => (
         <div className="box">
             <article>
@@ -37,18 +73,15 @@ const CardPage = (props) => {
                                 <img src={card.images.large}/>
                             </figure>
                         </div>
-                        <div className="column">
-                            <div className="block">
-                                <p className="is-6"><b>Mana cost</b></p>
-                                <p>{card.manaCost}</p>
-                            </div>
-                            <div className="block">
-                                <p className="is-6"><b>Type line</b></p>
-                                <p>{card.typeLine}</p>
-                            </div>
-                            <div className="block">
-                                <p className="is-6"><b>Oracle text</b></p>
-                                <p>{card.oracleText}</p>
+                        <div className="column card-info-columns">
+                            <div className="columns">
+                                <div className="column">
+                                    {renderCardInventory()}
+                                </div>
+                                <div className="column">
+                                    <hr className="column-separator"/>
+                                    {renderCardDetails()}
+                                </div>
                             </div>
                         </div>
                     </div>
