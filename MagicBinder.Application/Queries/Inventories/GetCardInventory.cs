2035f95a9538b0cccb79ed708c161d847b98d75b
Michał Sakwa <sakwa.michal@gmail.com>
2022-04-29 20:22:33 +0200

    Added inventory query.

---
+++ MagicBinder.Application/Queries/Inventories/GetCardInventory.cs
@@ -0,0 +1,22 @@
+﻿using MagicBinder.Application.Models.Inventories;
+using MagicBinder.Core.Requests;
+using MagicBinder.Infrastructure.Repositories;
+
+namespace MagicBinder.Application.Queries.Inventories;
+
+public record GetCardInventory(Guid OracleId) : Request<List<CardInventoryModel>>;
+
+public class GetCardInventoryHandler : RequestHandler<GetCardInventory, List<CardInventoryModel>>
+{
+    private readonly InventoriesRepository _inventoriesRepository;
+
+    public GetCardInventoryHandler(InventoriesRepository inventoriesRepository)
+    {
+        _inventoriesRepository = inventoriesRepository;
+    }
+    
+    public override async Task<RequestResult<List<CardInventoryModel>>> Handle(GetCardInventory request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
