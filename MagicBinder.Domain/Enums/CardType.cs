d36ddc919933201c284bb5088c7c4da1c4e83474
Michał Sakwa <sakwa.michal@gmail.com>
2022-05-07 00:39:00 +0200

    Added more card fields.

---
+++ MagicBinder.Domain/Enums/CardType.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicBinder.Domain.Enums;
+
+public enum CardType
+{
+    Other = 0,
+    Land = 1,
+    Creature = 2,
+    Artifact = 3,
+    Enchantment = 4,
+    Planeswalker = 5,
+    Instant = 6,
+    Sorcery = 7
+}
