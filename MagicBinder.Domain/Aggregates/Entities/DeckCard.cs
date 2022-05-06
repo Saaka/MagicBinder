f219a6dfaadcc3e756ab9acea17133f05112ea1e
Michał Sakwa <sakwa.michal@gmail.com>
2022-05-06 23:46:13 +0200

    Added more fields for deck card.

--- MagicBinder.Domain/Aggregates/Entities/DeckCard.cs
+++ MagicBinder.Domain/Aggregates/Entities/DeckCard.cs
@@ -1,4 +1,6 @@
-﻿namespace MagicBinder.Domain.Aggregates.Entities;
+﻿using MagicBinder.Domain.Enums;
+
+namespace MagicBinder.Domain.Aggregates.Entities;
 
 public class DeckCard
 {
@@ -6,4 +8,9 @@ public class DeckCard
     public Guid CardId { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Count { get; set; }
+    public decimal Cmc { get; set; }
+    public decimal CustomCmc { get; set; }
+    public string ManaCost { get; set; } = string.Empty;
+    public ColorType[] Colors { get; set; } = Array.Empty<ColorType>();
+    public ColorType[] ColorIdentity { get; set; } = Array.Empty<ColorType>();
 }
