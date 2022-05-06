a6317b98b4f64ee3ffd6071e7170546602781618
Michał Sakwa <sakwa.michal@gmail.com>
2022-05-07 00:24:47 +0200

    Added type line to deck card.

--- MagicBinder.Domain/Aggregates/Entities/DeckCard.cs
+++ MagicBinder.Domain/Aggregates/Entities/DeckCard.cs
@@ -7,6 +7,7 @@ public class DeckCard
     public Guid OracleId { get; set; }
     public Guid CardId { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string TypeLine { get; set; } = string.Empty;
     public int Count { get; set; }
     public decimal Cmc { get; set; }
     public decimal CustomCmc { get; set; }
