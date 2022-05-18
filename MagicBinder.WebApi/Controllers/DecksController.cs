1fb8b70eedca9518ad63c39c21635a263e674366
Michał Sakwa <sakwa.michal@gmail.com>
2022-05-18 20:44:02 +0200

    Added Decks controller.

---
+++ MagicBinder.WebApi/Controllers/DecksController.cs
@@ -0,0 +1,22 @@
+﻿using MagicBinder.Application.Commands.Decks;
+using MagicBinder.WebApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicBinder.WebApi.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class DecksController
+{
+    private readonly MediatorCommandSender _mediator;
+
+    public DecksController(MediatorCommandSender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Guid>> CreateDeck(CreateDeck command) => await _mediator.SendCommand(command);
+}
