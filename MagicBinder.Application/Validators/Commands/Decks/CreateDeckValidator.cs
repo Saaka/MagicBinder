53e8d282e3669c3988c59cc5ec0f2ea1d78e0a91
Michał Sakwa <sakwa.michal@gmail.com>
2022-05-18 20:43:49 +0200

    Added validator for CreateDeck command.

---
+++ MagicBinder.Application/Validators/Commands/Decks/CreateDeckValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MagicBinder.Application.Commands.Decks;
+using MagicBinder.Domain.Enums;
+
+namespace MagicBinder.Application.Validators.Commands.Decks;
+
+public class CreateDeckValidator: AbstractValidator<CreateDeck>
+{
+    public CreateDeckValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        RuleFor(x => x.Format).Must(x => x == FormatType.Commander);
+
+        RuleFor(x => x.GameType).Must(x => x == GameType.Paper);
+    }
+}
